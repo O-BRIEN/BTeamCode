932bd161b3edf1a07ac723ed85a19d7f8b0b27d2
O-BRIEN <61067368+O-BRIEN@users.noreply.github.com>
2020-02-14 16:43:01 -0500

    Add files via upload

---
+++ include/robot-config.h
@@ -0,0 +1,19 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor rightMotor;
+extern motor leftMotor;
+extern motor liftMotor;
+extern motor rightIntakeMotor;
+extern motor leftIntakeMotor;
+extern motor actuatorMotor;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
